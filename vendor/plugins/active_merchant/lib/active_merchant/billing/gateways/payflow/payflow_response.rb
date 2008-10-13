f11ab06ab79301e6c1cf90dbbf6c925474976755
T. Wenrich <karfas@gmail.com>
2008-10-13 20:45:46 +0200

    active_merchant is now a module

--- vendor/plugins/active_merchant/lib/active_merchant/billing/gateways/payflow/payflow_response.rb
+++
@@ -1,9 +0,0 @@
-module ActiveMerchant #:nodoc:
-  module Billing #:nodoc:
-    class PayflowResponse < Response
-      def profile_id
-        @params['profile_id']
-      end
-    end
-  end
-end
--- vendor/plugins/active_merchant/test/unit/integrations/two_checkout_module_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../../test_helper'
-
-class TwoCheckoutModuleTest < Test::Unit::TestCase
-  include ActiveMerchant::Billing::Integrations
-  
-  def test_notification_method
-    assert_instance_of TwoCheckout::Notification, TwoCheckout.notification('name=cody')
-  end
-end 
